bf49994c3e483346c363804be619e86a5410b789
Ryan SS <166767290+Ryan-S-S@users.noreply.github.com>
2025-08-25 10:53:17 -0300

    mudando o jogo

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1756129806,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/limite-sheet0.png",
+		"images/player1-sheet0.png",
+		"images/player1-sheet1.png",
+		"images/bloco-sheet0.png",
+		"images/bloco-sheet1.png",
+		"images/bloco-sheet2.png",
+		"images/player2-sheet0.png",
+		"images/player2-sheet1.png",
+		"images/cursor_bloco-sheet0.png",
+		"images/grav1-sheet0.png",
+		"images/sel_blocos-sheet0.png",
+		"images/cursor_mouse-sheet0.png",
+		"images/selecionador-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
